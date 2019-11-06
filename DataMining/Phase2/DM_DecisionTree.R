d20b42b76400132930639fbe8f7fca387dd5061c
Qianjx <qjx10@mail.ustc.edu.cn>
2019-11-06 00:29:31 -0500

    Update DM_DecisionTree.R

--- DataMining/Phase2/DM_DecisionTree.R
+++ DataMining/Phase2/DM_DecisionTree.R
@@ -5,26 +5,27 @@ library(sqldf)
 library(rpart)
 
 mean(Airb$price)
-price_x=Airb$price>150
+price_x=Airb$price>100
 Airb=data.frame(Airb,price_x)
 
-TRain=sqldf("select longitude,latitude,price,minimum_nights,
+TRain=sqldf("select longitude,neighbourhood_group,latitude,price,minimum_nights,
             availability_365,price_x from Airb where price!=0 and
-            price<1000 and id%10!=0")
+            price<1000 and id%5!=0")
 
 
-TEst=sqldf("select longitude,latitude,price,minimum_nights,
+TEst=sqldf("select longitude,neighbourhood_group,latitude,price,minimum_nights,
             availability_365,price_x from Airb where price!=0 and
-           price<1000 and id%10==0")
-
+           price<1000 and id%5==0")
+mean(TEst$price)
 # grow tree
-fit = rpart(price~longitude+latitude+minimum_nights+
-            availability_365,method="anova", data=TRain)
+fit = rpart(price_x~longitude+latitude+minimum_nights+
+            availability_365,method="class", data=TRain)
 
 printcp(fit) # display the results
 plotcp(fit) # visualize cross-validation results
 summary(fit) # detailed summary of splits
 plot(fit) # plot tree
 
-
+predtree=predict(fit,newdata=TEst,type="class")
+table(TEst$price_x,predtree)
 
