f11feaf2b42686f4d4f74036cb83eee1030490f5
Qianjx <qjx10@mail.ustc.edu.cn>
2019-11-10 18:23:01 -0500

    picture modification

--- DataMining/Phase2/line_diagram.R
+++ DataMining/Phase2/line_diagram.R
@@ -1,3 +1,4 @@
+x=c(50,100,150,200,250)
 knn=c(0.87,0.65,0.72,0.83,0.90)
 DT=c(0.87,0.72,0.74,0.84,0.90)
 NB=c(0.84,0.68,0.72,0.84,0.89)
@@ -6,10 +7,10 @@ NB=c(0.84,0.68,0.72,0.84,0.89)
 png(file = "line_diagram.jpg")
 
 # Plot the bar chart.
-plot(knn,type = "o",col = "red",
+plot(x,knn,type = "o",col = "red",
      main = "3 different algorithms")
 
-lines(DT, type = "o", col = "blue")
-lines(NB, type = "o", col = "yellow")
+lines(x,DT, type = "o", col = "blue")
+lines(x,NB, type = "o", col = "yellow")
 # Save the file.
 dev.off()
